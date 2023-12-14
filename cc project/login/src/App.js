47726f63c8c21c7bb898d9aca32902dc207b18da
TEJASVI S <118422301+tejasvis07@users.noreply.github.com>
2023-12-14 10:10:41 +0530

    Add files via upload

---
+++ cc project/login/src/App.js
@@ -0,0 +1,13 @@
+// App.js
+import React from 'react';
+import SignUpForm from './components/SignUpForm';
+
+function App() {
+  return (
+    <div className="App">
+      <SignUpForm />
+    </div>
+  );
+}
+
+export default App;
