08f331ba7b5b354a1a2fd8914f897c52968c482e
TEJASVI S <118422301+tejasvis07@users.noreply.github.com>
2023-12-14 10:15:12 +0530

    Add files via upload

---
+++ cc project/registration/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReservationForm from './components/ReservationForm';
+
+const App = () => {
+  return (
+    <div>
+      
+      <ReservationForm />
+    </div>
+  );
+};
+
+export default App;
